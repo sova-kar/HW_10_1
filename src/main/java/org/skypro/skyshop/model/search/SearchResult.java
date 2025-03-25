3ff41527956f16af09fdd085d4e0146ef3a69a99
SOVA <sova16tais@gmail.com>
2025-03-25 21:47:36 +0300

    I did my homework 10

---
+++ src/main/java/org/skypro/skyshop/model/search/SearchResult.java
@@ -0,0 +1,35 @@
+package org.skypro.skyshop.model.search;
+
+import java.util.UUID;
+
+public class SearchResult {
+    private final String id;
+    private final String name;
+    private final String contentType;
+
+    public SearchResult(String id, String name, String contentType) {
+        this.id = id;
+        this.name = name;
+        this.contentType = contentType;
+    }
+
+    public static SearchResult fromSearchable(Searchable searchable) {
+        return new SearchResult(
+                searchable.getId().toString(),
+                searchable.getSearchTerm(),
+                searchable.getContentType()
+        );
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+}
